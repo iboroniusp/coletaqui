8f58706a242b20852a7fcf8561c81d3f58031498
luna <tiago.luna@easynvest.com.br>
2020-10-12 13:08:48 -0300

    alteração da página de cadastro de coleta e acréscimo da página de login

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -9,12 +9,14 @@ import { Route, BrowserRouter } from 'react-router-dom';
 
 import Home from './pages/Home';
 import CreatePoint from './pages/CreatePoint';
+import Login from './pages/Login';
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Route component={Home} path="/" exact />
             <Route component={CreatePoint} path="/create-point" />
+            <Route component={Login} path="/login" />
         </BrowserRouter>
     );
 }
